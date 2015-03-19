9eccd11aee0cc51af40397c4a037c42340ef21f8
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2015-03-20 00:59:08 +0100

    (maint) Correct indentation

--- lib/puppet_x/puppetlabs/migration/migration_issues.rb
+++ lib/puppet_x/puppetlabs/migration/migration_issues.rb
@@ -10,10 +10,10 @@ module PuppetX::Puppetlabs::Migration::MigrationIssues
   MIGRATE4_AMBIGOUS_INTEGER = issue :MIGRATE4_AMBIGOUS_INTEGER, :value, :radix do
     formatted = sprintf("'%##{radix == 8 ? 'o' : 'x'}'", value)
     "This #{radix == 8 ? 'octal' : 'hex'} value evaluates to the decimal value '#{value}': Quote if the String #{formatted} was intended."
-    end
+  end
 
   MIGRATE4_AMBIGOUS_FLOAT = issue :MIGRATE4_AMBIGOUS_FLOAT, :value do
     "This value evaluates to the imprecise floating point number #{value}: Quote if a String value was intended"
-    end
+  end
 
 end
