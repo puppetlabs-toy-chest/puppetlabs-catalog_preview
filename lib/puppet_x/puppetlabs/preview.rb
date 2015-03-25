fbc6fb2929b73bb677ce30e1436647f1f7a677bf
Thomas Hallgren <thomas@tada.se>
2015-03-25 18:58:44 +0100

    (PUP-4280) Implement Catalog Diff
    Adds a model structure containing the elements needed to present
    a CatalogDelta. The constructor of the CatalogDelta instance is
    capable of calculating the delta based on two hash inputs.
    
    This is work in progress and this commmit should be considered
    as a functional preview.

--- lib/puppet_x/puppetlabs/preview.rb
+++ lib/puppet_x/puppetlabs/preview.rb
@@ -6,6 +6,7 @@ module PuppetX
     module Migration
       require 'puppet_x/puppetlabs/migration/migration_issues'
       require 'puppet_x/puppetlabs/migration/migration_checker'
+      require 'puppet_x/puppetlabs/migration/model'
     end
   end
 end
