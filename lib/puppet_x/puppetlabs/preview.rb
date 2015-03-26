4fa2eff70753da1398f479f667cd7c0015cb07d8
Hailee Kenney <hailee@puppetlabs.com>
2015-03-26 14:45:40 -0700

    (PUP-4122) Add validation of migrate option
    Enure that when the --migrate option is used that the baseline
    environment does not have future parser enabled and that the
    preview environment does

--- lib/puppet_x/puppetlabs/preview.rb
+++ lib/puppet_x/puppetlabs/preview.rb
@@ -7,6 +7,7 @@ module PuppetX
       require 'puppet_x/puppetlabs/migration/migration_issues'
       require 'puppet_x/puppetlabs/migration/migration_checker'
       require 'puppet_x/puppetlabs/migration/catalog_delta_model'
+      require 'puppet_x/puppetlabs/migration/errors'
     end
   end
 end
