56f502b86b57822c2bc8868ca4e17fd1749591ae
Hailee Kenney <hailee@puppetlabs.com>
2015-06-24 16:31:07 -0700

    (PRE-47) Add tests for --last warnings
    Add tests to ensure that --last warns if you attempt to run with
    an empty preview output directory or if you ask for data for a node
    that does not have a directory in the preview output directory.

--- spec/unit/preview_spec.rb
+++ spec/unit/preview_spec.rb
@@ -8,6 +8,34 @@ require 'json'
 
 describe Puppet::Application::Preview do
 
+  context "when running with --last" do
+    let (:preview) { Puppet::Application[:preview] }
+
+    it "warns if there is no preview data in the specified output direcotry" do
+      preview.options[:last] = true
+      Puppet[:preview_outputdir] = "/test/dir"
+      stub_outputdir_contents([])
+
+      expected_error = "There is no preview data in the specified output directory '/test/dir', you must have data from a previous preview run to use --last"
+
+      expect{ preview.run_command }.to raise_error(RuntimeError, expected_error)
+    end
+
+    it "warns if there is no directory for the specified node" do
+      stub_outputdir_contents(["fake/path/dir_a", "fake/path/dir_b"])
+      preview.options[:nodes] = ['some.node.com']
+      preview.options[:last] = true
+
+      expected_error = "There is no preview data available for node 'some.node.com'. It is likely this node has not been compiled with preview."
+
+      expect { preview.run_command}.to raise_error(RuntimeError, expected_error)
+    end
+
+    def stub_outputdir_contents(directories)
+      Dir.stubs(:[]).with("#{Puppet[:preview_outputdir]}/*").returns directories
+    end
+  end
+
   context "when running with --view" do
     let(:catalog_delta) { PuppetX::Puppetlabs::Migration::CatalogDeltaModel::CatalogDelta }
 
